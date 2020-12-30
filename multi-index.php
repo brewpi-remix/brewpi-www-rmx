b383fe85ca3231a44071b882f313f8967b1e2f18
lbussy <lee@bussy.org>
2020-12-30 17:15:35 -0600

    Complete re-write

--- multi-index.php
+++ multi-index.php
@@ -15,74 +15,53 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with BrewPi WWW RMX. If not, see <https://www.gnu.org/licenses/>.
- *
- * These scripts were originally a part of brewpi-www, a part of
- * the BrewPi project. Legacy support (for the very popular Arduino
- * controller) seems to have been discontinued in favor of new hardware.
- *
- * All credit for the original brewpi-www goes to @elcojacobs,
- * @lukepower, @m-mcgowan, @vanosg, @GregAtkinson and I'm sure
- * many more contributors around the world. My apologies if I have
- * missed anyone; those were the names listed as contributors on the
- * Legacy branch.
- *
- * See: 'original-license.md' for notes about the original project's
- * license and credits. */
+*/
+
+// TODO:  Remove
+clearstatcache();
 
 $columns = 3; // Number of columns in display table
+$chamberLoc = "";
 
 function getFileList($dir, $recurse = FALSE, $depth = FALSE)
 {
-  $retval = [];
+    $retval = [];
 
-  // Add trailing slash if missing
-  if(substr($dir, -1) != "/") {
-    $dir .= "/";
-  }
-
-  // Open pointer to directory and read list of files
-  $d = @dir($dir) or die("getFileList: Failed opening directory {$dir} for reading");
-  while(FALSE !== ($entry = $d->read())) {
-    // Skip hidden files
-    if($entry{0} == ".") continue;
-    if(is_dir("{$dir}{$entry}")) {
-      // Skip directory;
-      if($recurse && is_readable("{$dir}{$entry}/")) {
-        if($depth === FALSE) {
-          $retval = array_merge($retval, getFileList("{$dir}{$entry}/", TRUE));
-        } elseif($depth > 0) {
-          $retval = array_merge($retval, getFileList("{$dir}{$entry}/", TRUE, $depth-1));
-        }
-      }
-    } elseif(is_readable("{$dir}{$entry}")) {
-      if ($entry == 'lcd.php') {
-        $retval[] = [
-          'name' => "{$dir}{$entry}",
-        ];
-      }
+    // Add trailing slash if missing
+    if (substr($dir, -1) != "/") {
+        $dir .= "/";
     }
-  }
-  $d->close();
-  return $retval;
+
+    // Open pointer to directory and read list of files
+    $d = @dir($dir) or die("getFileList: Failed opening directory {$dir} for reading");
+    while (FALSE !== ($entry = $d->read())) {
+        // Skip hidden files
+        if ($entry{0} == ".") continue;
+        if (is_dir("{$dir}{$entry}")) {
+            // Skip directory;
+            if ($recurse && is_readable("{$dir}{$entry}/")) {
+                if ($depth === FALSE) {
+                    $retval = array_merge($retval, getFileList("{$dir}{$entry}/", TRUE));
+                } elseif($depth > 0) {
+                    $retval = array_merge($retval, getFileList("{$dir}{$entry}/", TRUE, $depth-1));
+                }
+            }
+        } elseif(is_readable("{$dir}{$entry}")) {
+            if ($entry == 'lcd.php') {
+                $retval[] = [
+                    'name' => "{$dir}{$entry}",
+                ];
+            }
+        }
+    }
+    $d->close();
+    return $retval;
 }
 
- // See if we are using an IP to access page, and/or if user is on Windows
- $ipurl = (filter_var($_SERVER['HTTP_HOST'], FILTER_VALIDATE_IP) ? true : false);
- $windows = (preg_match('/windows|win32/i', $_SERVER['HTTP_USER_AGENT']) ? true : false);
- // Form URL with host name
- $named_url = 'http://' . gethostname() . '.local' . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-// Bonjour prompt
-$bjprompt = '';
-if ($ipurl && $windows) {
-    $bjprompt .= "<div id=\"bonjour-panel\" class=\"ui-widget ui-widget-content ui-corner-all\">\r\n";
-    $bjprompt .= "<div id=\"top-bar\" class=\"ui-widget ui-widget-header ui-corner-all\">\r\n";
-    $bjprompt .= "<a href=\"https://support.apple.com/kb/DL999\">\r\n";
-    $bjprompt .= "<img style=\"float: left;\" src=\"images/bonjour.png\" alt=\"Bonjour icon\" width=\"43\" /></a>\r\n";
-    $bjprompt .= "<p>&nbsp;You are using an IP to access your BrewPi.\r\n";
-    $bjprompt .= "You can use <a href=\"" . $named_url . "\">" . $named_url . "</a> instead\r\n";
-    $bjprompt .= "if you install <a href=\"https://support.apple.com/kb/DL999\">Bonjour from Apple</a>.\r\n";
-    $bjprompt .= "</div>\r\n</div>";
+function htmlpath($relative_path) {
+    $realpath=realpath($relative_path);
+    $htmlpath=str_replace($_SERVER['DOCUMENT_ROOT'],'',$realpath);
+    return $htmlpath;
 }
 
 // Create table of frames
@@ -93,6 +72,10 @@ $dyn_table = '<table summary="Multi-Chamber Display" class="lcd-table" cellpaddi
 $dirlist = getFileList("./", TRUE, 1);
 // Sort the array
 sort($dirlist);
+// Get a chamber location
+$chamberLoc = htmlpath(pathinfo($dirlist[0]['name'])['dirname']);
+$chamberLoc = ltrim($chamberLoc,'/');
+
 foreach($dirlist as $file) {
     $dyn_table .= ($i % $columns == 0 ? '<tr>' . "\n" : ''); // Make a table up to $columns columns wide
     $dyn_table .= '<td>' . '<iframe src="' . $file['name'] . '" scrolling="no" width="312" height="135">' . "\n";
@@ -105,84 +88,40 @@ $dyn_table .= ($i % $columns == 0 ? '' : '</tr>' . "\n"); // Close row if it has
 $dyn_table .= '<tr><td></td></tr>' . "\n"; // Hack to pad the bottom of lcd-panel to allow dynamic height
 $dyn_table .= '</table>';
 
-// Get correct logo
-list($scriptPath) = get_included_files();
-$docloc = str_replace($_SERVER['DOCUMENT_ROOT'],'',dirname($scriptPath));
-$imgloc = $docloc . '/images';
-$custom = $imgloc . '/custom_logo.png';
-$stock = $imgloc . '/brewpi_logo.png';
-$css = $docloc . '/css'; // Get css location
-$logo = (file_exists($_SERVER['DOCUMENT_ROOT'] . $custom) ? $custom : $stock);
-$logo = '<img class="logo" src="' . $logo . '">';
-
-// Git information for footer
-$docloc = str_replace($_SERVER['DOCUMENT_ROOT'],'',dirname($scriptPath));
-$tbwd = getcwd();
-chdir($_SERVER['DOCUMENT_ROOT'] . $GLOBALS['docloc']);
-$version = trim(shell_exec('git describe --tags $(git rev-list --tags --max-count=1)'));
-$branch = trim(shell_exec('git branch | grep \* | cut -d " " -f2'));
-$commit = trim(shell_exec('git -C . log --oneline -n1'));
-chdir($tbwd);
-$arr = explode(' ', trim($commit));
-$commit = "[ <span class=\"version-text-mono\">";
-$loop = '';
-foreach ($arr as $key => $word) {
-    if ($key == 0) { // Make commit hash yellow
-        $loop = "<span class=\"version-text-monoylw\">" . $word . "</span> - ";
-    } else {
-        $loop .= $word . " ";
-    }
-}
-$commit .= trim($loop) . "</span> ]";
-$division = "<div id=\"version-panel\" class=\"ui-widget ui-widget-content ui-corner-all\">\r\n";
-$division .= "<div id=\"bottom-bar\" class=\"ui-widget ui-widget-header ui-corner-all\">\r\n";
-$division .= "<div id=\"version-text\">\r\n";
-$division .= "<span>BrewPi Remix version: " . trim($version) . " (" . trim($branch) . ")</span>\r\n";
-$division .= trim($commit) . "\r\n";
-$division .= "</div>\r\n</div>\r\n</div>";
-$gitinfo = $division;
-
 $title = 'BLR: Chamber Dashboard';
 
 ?>
-<!DOCTYPE html >
+<!DOCTYPE html>
 <html>
+
 <head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<title><?php echo $title; ?></title>
-<link type="text/css" href="<?php echo $css ?>/redmond/jquery-ui-1.10.3.custom.css" rel="stylesheet" />
-<link rel="stylesheet" type="text/css" href="<?php echo $css ?>/style.css" />
-<link rel="apple-touch-icon" href="<?php echo $imgloc . '/'; ?>touch-icon-iphone.png">
-<link rel="apple-touch-icon" sizes="76x76" href="<?php echo $imgloc . '/'; ?>touch-icon-ipad.png">
-<link rel="apple-touch-icon" sizes="120x120" href="<?php echo $imgloc . '/'; ?>touch-icon-iphone-retina.png">
-<link rel="apple-touch-icon" sizes="152x152" href="<?php echo $imgloc . '/'; ?>touch-icon-ipad-retina.png">
-<meta name="apple-mobile-web-app-title" content="<?php echo $title; ?>">
-<link rel="icon" type="image/png" href="<?php echo $imgloc . '/'; ?>favicon.ico">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title><?php echo $title; ?></title>
+    <link type="text/css" href="<?php echo $chamberLoc . '/'; ?>css/redmond/jquery-ui-1.10.3.custom.css" rel="stylesheet" />
+    <link type="text/css" href="<?php echo $chamberLoc . '/'; ?>css/style.css" rel="stylesheet" />
+    <link type="text/css" href="<?php echo $chamberLoc . '/'; ?>css/multi-index.css" rel="stylesheet" />
+    <link rel="apple-touch-icon" href="<?php echo $chamberLoc . '/'; ?>images/touch-icon-iphone.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo $chamberLoc . '/'; ?>images/touch-icon-ipad.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo $chamberLoc . '/'; ?>images/touch-icon-iphone-retina.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo $chamberLoc . '/'; ?>images/touch-icon-ipad-retina.png">
+    <meta name="apple-mobile-web-app-title" content="<?php echo $title; ?>">
+    <link rel="icon" type="image/png" href="<?php echo $chamberLoc . '/'; ?>images/favicon.ico">
 </head>
+
 <body>
 
-<!-- Bonjour prompt bar start -->
-<?php echo $bjprompt; ?>
-<!-- Bonjour prompt bar end -->
+<?php include 'get-bjprompt.php'; ?>
 
-<div id="lcd-logo-panel" class="ui-widget ui-widget-content ui-corner-all">
-    <div id="lcd-top-bar" class="ui-widget ui-widget-header ui-corner-all">
-    	<div id="lcd-logo-container">
-    		<div id=lcd-name-container>
-                <?php echo $logo; ?>
-                <span class="lcd-title">Multi-Chamber Dashboard</span>
-    		</div>
-    	</div>
-    </div>
+<div id="beer-panel" class="ui-widget ui-widget-content ui-corner-all">
+        <div id="top-bar" class="grid-container ui-widget ui-widget-header ui-corner-all">
+            <div class="m-logo"><?php include 'get-logo.php'; ?></div>
+            <div class="m-title">Multi-Chamber Dashboard</div>
+        </div>
+
+        <?php echo $dyn_table; ?>
 </div>
 
-<div id="lcd-panel" class="ui-widget ui-widget-content ui-corner-all">
-    <?php echo $dyn_table; ?>
-</div>
-
-<!-- Git version bar start -->
-<?php echo $gitinfo; ?>
-<!-- Git version bar end -->
+<?php include 'get-gitinfo.php'; ?>
 
 </body>
 </html>
